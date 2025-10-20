20e4ad786ff58b3b3e72aaae85c8ac69698af3d5
BinaryKenny <khairullinvadim2006@gmail.com>
2025-10-20 12:39:38 +0300

    The Functions for creating and removing matrix was extended

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
+
+int ** create(size_t rows, size_t cols);
+
+void remove (int **matrix, size_t rows, size_t cols);
+
 int main()
 {
+	
+	
+
 	size_t rows = 0, cols = 0;
 	std::cin >> rows >> cols;
 	if (!std::cin){
 		return 1;}
 	std::cout << rows * cols << "\n";
+	int ** matrix = create(rows, cols);
 
+	remove(matrix, rows, cols);
 }	
