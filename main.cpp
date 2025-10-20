640c07aecbc187c8bb0db93713575aa5a7b64037
BinaryKenny <khairullinvadim2006@gmail.com>
2025-10-20 12:47:45 +0300

    Main() was extended

--- main.cpp
+++ main.cpp
@@ -4,17 +4,23 @@ int ** create(size_t rows, size_t cols);
 
 void remove (int **matrix, size_t rows, size_t cols);
 
+void input(int ** matrix, size_t rows, size_t cols);
+
+void output(const int * const * matrix, size_t rows, size_t cols);
 int main()
 {
-	
-	
-
 	size_t rows = 0, cols = 0;
 	std::cin >> rows >> cols;
 	if (!std::cin){
 		return 1;}
 	std::cout << rows * cols << "\n";
+
+
 	int ** matrix = create(rows, cols);
+	input (matrix, rows, cols);
+	output(matrix, rows, cols);
 
 	remove(matrix, rows, cols);
+
+	std::cout << "\n";	
 }	
