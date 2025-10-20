ab9894660ab0e815b0b208ba60c84bb53b4fdccb
BinaryKenny <khairullinvadim2006@gmail.com>
2025-10-20 12:54:47 +0300

    Extended input()

--- main.cpp
+++ main.cpp
@@ -4,20 +4,32 @@ int ** create(size_t rows, size_t cols);
 
 void remove (int **matrix, size_t rows, size_t cols);
 
-void input(int ** matrix, size_t rows, size_t cols);
+void input(int ** matrix, size_t rows, size_t cols){
+	for (size_t i = 0; i < rows && std::cin; ++i){
+		for (size_t j = 0; i < cols && std::cin;++j){
+			std::cin >> matrix[i][j];
+		}
+	}
+} 
 
 void output(const int * const * matrix, size_t rows, size_t cols);
+
 int main()
 {
 	size_t rows = 0, cols = 0;
 	std::cin >> rows >> cols;
 	if (!std::cin){
+		remove(matrix, rows, cols);
 		return 1;}
 	std::cout << rows * cols << "\n";
 
 
 	int ** matrix = create(rows, cols);
 	input (matrix, rows, cols);
+	if (!std::cin){
+		remove(matrix, rows, cols);
+		return 1;
+	}
 	output(matrix, rows, cols);
 
 	remove(matrix, rows, cols);
